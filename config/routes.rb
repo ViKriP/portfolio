7b5a2446e93fdaecca09d56167e568306779baff
ViKriP <viktor_biz@ukr.net>
2020-03-02 15:25:31 +0200

    updated gems and added ssl

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,6 @@ Rails.application.routes.draw do
   #  get('users/checkout_login', to: 'users/registrations#checkout_login', as: 'checkout_login')
   #  get '/auth/failure', to: 'users/omniauth_callbacks#failure', as: 'oauth_failure'
   #end
+
+  get '/viktor', to: 'homepage#index', as: 'viktor'
 end
